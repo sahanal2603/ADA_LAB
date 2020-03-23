299942957d3667f5481b55379b5a876742985a8f
sahanal2603 <53811265+sahanal2603@users.noreply.github.com>
2020-03-23 16:07:23 +0530

    Add files via upload

---
+++ 13b_Sum_of_even_no._of_1s.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+using namespace std;
+
+bool even(int n)
+{
+	int i;
+	for(i=0; n>0; i++)
+	n = n & (n-1);
+	if(i%2==0)
+	return true;
+	return false;
+}
+
+int sum(int n)
+{
+	int sum = 0;
+	while(n>0)
+	{
+		sum += n % 10;
+		n = n / 10;
+	}
+	return sum;
+}
+
+
+int main()
+{
+	cout<<"Enter the size of array: ";
+	int n, i;
+	cin>>n;
+	cout<<"Enter the array: ";
+	int arr[n];
+	for(i=0; i<n; i++)
+	cin>>arr[i];
+	int S = 0;
+	for(i=0; i<n; i++)
+	{
+		if(even(arr[i]))
+		S += sum(arr[i]);
+	}
+	cout<<"Sum is: "<<S;
+}
+
